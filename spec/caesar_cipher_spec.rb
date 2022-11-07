58d6c4cff6e70346ffff30b19dc8d8ae0fc311c7
DanielT94 <daniel.turnbull94@gmail.com>
2022-11-07 12:41:54 +0000

    Initialised RSpec

---
+++ spec/caesar_cipher_spec.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require './lib/caesar_cipher.rb'
+
+describe CaesarCipher do
+  subject { CaesarCipher.new }
+
+  #works with small positive shift
+
+  #works with small negative shift
+
+  #works with large positive shift
+
+  #works with large negative shift
+
+  #works with punctuation and spaces
+
+end
