a096e79f81e1c3c1adc16f9e6715c9a38be56a8b
DanielT94 <daniel.turnbull94@gmail.com>
2022-11-07 14:12:05 +0000

    Corrected indentation

--- lib/caesar_cipher.rb
+++ lib/caesar_cipher.rb
@@ -1,24 +1,24 @@
 # frozen_string_literal: true
 
-#performs shift on string
+# performs shift on string
 class CaesarCipher
-    def shift(string, shift_factor = 0)
-      result = ''
-      string.each_char do |char|
-        # use shift on charcters only if they're letters
-        if char.ord.between?(65, 90) || char.ord.between?(97, 122)
-          new_ord = char.ord + shift_factor
-          # wrap from Z to A or z to a
-          new_ord -= 26 if (new_ord > 91 && char.ord < 91) || new_ord > 122
-          result += new_ord.chr
-        else
-          result += char
-        end
+  def shift(string, shift_factor = 0)
+    result = ''
+    string.each_char do |char|
+      # use shift on charcters only if they're letters
+      if char.ord.between?(65, 90) || char.ord.between?(97, 122)
+        new_ord = char.ord + shift_factor
+        # wrap from Z to A or z to a
+        new_ord -= 26 if (new_ord > 91 && char.ord < 91) || new_ord > 122
+        result += new_ord.chr
+      else
+        result += char
       end
-      result
     end
+    result
   end
+end
   
-  translation = CaesarCipher.new
-  p translation.shift('What a string!', 5)
+translation = CaesarCipher.new
+p translation.shift('What a string!', 5)
   
